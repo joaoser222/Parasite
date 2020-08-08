bd6966acb99e81f1e0e5b438987ac3d9a6d8a9f6
joaoser222 <joaoser222@gmail.com>
2020-08-08 08:47:40 -0300

    Mod. rotas

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,11 +2,24 @@
 const routes = [
   {
     path: '/',
+    name:'Home',
+
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '/requests',name: 'Requisições',icon: 'eva-swap', component: () => import('pages/Requests.vue') },
     ]
+
   },
+  {
+    path: '/debugger',
+    name:'Depurador',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '',name:'Depurador', component: () => import('pages/Debugger.vue') },
+    ]
+
+  },
+
 
   // Always leave this as last one,
   // but you can also remove it
